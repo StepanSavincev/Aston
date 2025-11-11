8f93a110dbe138d72d1ea830ee5f34365765d830
StepanSav <set.savincev@yandex.ru>
2025-11-11 15:36:14 +0300

    Lesson_2.5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,159 +1,34 @@
 package org.example;
 
-import java.util.Scanner;
-import java.util.Arrays;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        printThreeWorld();
-        checkSumSign();
-        printColor();
-        compareNumbers();
-        checkSumInRange();
-        checkPositiveOrNegative();
-        System.out.println(isNegative(-2));
-        printStringMultipleTimes("Строка", 5);
-        System.out.println(isLeapYear(2001));
-        invertArray(new int[]{1, 1, 0, 0, 1, 0, 1, 1, 0, 0});
-        fillArrWithSequence();
-        multiplyLessThanSix();
-        fillDiagonalArray();
-        int[] resultArray = createArray(5, 10);
-        System.out.println(Arrays.toString(resultArray));
-    }
-    // 1
-    public static void printThreeWorld() {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
-    }
-    // 2
-    public static void checkSumSign() {
-        int a = 3;
-        int b = 5;
-        int sum = a + b;
-        if (sum >= 0) {
-            System.out.println("Сумма положительная");
-        } else {
-            System.out.println("Сумма отрицательная");
-        }
-    }
-    // 3
-    public static void printColor() {
-        System.out.println("\nЗадание 3:");
-        int value = 3;
-        if (value <= 0) {
-            System.out.println("Красный");
-        } else if (value <= 100) {
-            System.out.println("Желтый");
-        } else {
-            System.out.println("Зеленый");
-        }
-    }
+        Map<String, Double> grades1 = Map.of("Литература", 4.0, "Физ-ра", 3.5);
+        Map<String, Double> grades2 = Map.of("Литература", 2.0, "Физ-ра", 2.5);
 
-    // 4
-    public static void compareNumbers() {
-        int a = 3;
-        int b = 5;
-        if (a >= b) {
-            System.out.println("a>=b");
-        } else {
-            System.out.println("a<b");
-        }
-    }
+        Set<Student> students = new HashSet<>();
+        students.add(new Student("Иванов", "121", 1, grades1));
+        students.add(new Student("Петров", "415", 1, grades2));
 
-    // 5
-    public static boolean checkSumInRange() {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Введите первое число: ");
-        int num1 = scan.nextInt();
-        System.out.println("Введите второе число: ");
-        int num2 = scan.nextInt();
-        int res;
-        res = num1 + num2;
-        return res >= 10 && res <= 20;
-    }
+        Student.removeLowGradeStudents(students);
+        Student.promoteStudents(students);
+        Student.printStudents(students, 2);
+        PhoneBook phoneBook = new PhoneBook();
 
-    // 6
-    public static void checkPositiveOrNegative() {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Введите число: ");
-        int num1 = scan.nextInt();
-        if (num1 >= 0) {
-            System.out.println("Число положительное");
-        } else {
-            System.out.println("Число отрицательное");
-        }
-    }
+        // Добавляем записи в телефонный справочник
+        phoneBook.add("Иванов", "89001234567");
+        phoneBook.add("Петров", "89001234577");
+        phoneBook.add("Александров", "89001234587");
+        phoneBook.add("Сидоров", "89001234597");
+        phoneBook.add("Петров", "89001234507");
 
-    // 7
-    public static boolean isNegative(int number) {
-        return number < 0;}
+        // Получаем номера по фамилии
+        System.out.println("Номера телефонов для Иванова: " + phoneBook.get("Иванов"));
+        System.out.println("Номера телефонов для Петрова: " + phoneBook.get("Петров"));
 
-    // 8
-    public static void printStringMultipleTimes(String str, int times){
-        if (times < 0) {
-            System.out.println("Количество повторений не может быть отрицательным.");
-            return;
-        }
-        for (int i = 0; i < times; i++) {
-            System.out.println(str);
-        }
-    }
-
-    // 9
-    public static boolean isLeapYear(int year) {
-        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-    }
-
-    // 10
-    public static void invertArray(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (arr[i] == 0) ? 1 : 0;
-            System.out.print(arr[i] + " ");
-        }
-    }
-
-    // 11
-    public static void fillArrWithSequence(){
-        int [] arr = new int[100];
-        for (int i = 0; i < arr.length; i++){
-            arr[i]= i + 1;
-            System.out.print(arr[i] + " ");
-        }
-    }
-
-    // 12
-    public static void multiplyLessThanSix() {
-        int [] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 11};
-        for (int i =0; i < arr.length; i++) {
-            if (arr[ i ] < 6) {
-                arr[i] *= 2;
-            }
-            System.out.print(arr[i] + " ");
-        }
-    }
-
-    // 13
-    public static void fillDiagonalArray() {
-        int size = 4;
-        int[][] array = new int[size][size];
-        for (int i = 0; i < size * size; i++) {
-            int row = i / size;
-            int col = i % size;
-            array[row][col] = (row == col) ? 1 : 0;
-            System.out.print(array[row][col] + " ");
-            if (col == size - 1) {
-                System.out.println();
-            }
-        }
-    }
-
-    // 14
-    public static int[] createArray(int len, int initialValue) {
-        int[] array = new int[len];
-        for (int i = 0; i < len; i++) {
-            array[i] = initialValue;
-        }
-        return array;
+        // Вывод всех записей (для проверки)
+        System.out.println("\nВсе записи в телефонном справочнике:");
+        phoneBook.printAll();
     }
 }
---
+++ src/main/java/org/example/PhoneBook.java
@@ -0,0 +1,25 @@
+package org.example;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PhoneBook {
+    private Map<String, List<String>> phoneBook;
+
+    public PhoneBook() {
+        phoneBook = new HashMap<>();
+    }
+    public void add(String surname, String phoneNumber) {
+        phoneBook.putIfAbsent(surname, new ArrayList<>());
+        phoneBook.get(surname).add(phoneNumber);
+    }
+    public List<String> get(String surname) {
+        return phoneBook.getOrDefault(surname, new ArrayList<>());
+    }
+    public void printAll() {
+        for (Map.Entry<String, List<String>> entry : phoneBook.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+}
